bbc58a9d5ea43979295b6877ed0d16fc3cec86f1
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2020-01-14 10:18:21 -0600

    BOD - 01/14/20 - 01
    @tri-vi
    
    - Conditional Rendering

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var app = new Vue({
     description: "This is a sock description",
     image: './assets/image_1.jpg',
     altText: 'altText',
-    hrefLink: ''
+    hrefLink: '',
+    inventory: 100,
+    onSale: true
   }
 });
