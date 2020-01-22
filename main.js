d428335f35a9122604cc026460d2eb5883ade14c
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2020-01-22 14:54:52 -0600

    MOD - 01/22/20
    @tri-vi
    
    - Update computed properties

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: "LUNA",
     product: "Product",
-    image: './assets/image_2.jpg',
+    selectedIndex: 0,
     altText: 'altText',
     hrefLink: '',
-    inventory: 0,
-    inStock: false,
-    onSale: true,
     details: ["detail 1", "detail 2", "detail 3"],
     variants: [
       {
         variantId: 1234,
         variantColor: "green",
-        variantImage: './assets/image_1.jpg'
+        variantImage: './assets/image_1.jpg',
+        variantQuantity: 100,
+        variantOnSale: false
       },
       {
         variantId: 1235,
         variantColor: "blue",
-        variantImage: './assets/image_2.jpg'
+        variantImage: './assets/image_2.jpg',
+        variantQuantity: 5,
+        variantOnSale: true
       },
       {
         variantId: 1236,
         variantColor: "orange",
-        variantImage: './assets/image_3.jpg'
+        variantImage: './assets/image_3.jpg',
+        variantQuantity: 0,
+        variantOnSale: false
       }
     ],
     cart: 0,
@@ -32,14 +36,31 @@ var app = new Vue({
     addToCart(){
       this.cart +=1;
     },
-    updateProduct(variantImage){
-      this.image = variantImage;
+    updateProduct(index){
+      this.selectedIndex = index;
     },
     removeFromCart(){
       if(this.cart > 0){
         this.cart -= 1;
       }
       
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image(){
+      return this.variants[this.selectedIndex].variantImage;
+    },
+    inventory(){
+      return this.variants[this.selectedIndex].variantQuantity;
+    },
+    inStock(){
+      return this.variants[this.selectedIndex].variantQuantity;
+    },
+    onSale(){
+      return this.variants[this.selectedIndex].variantOnSale;
     }
   }
 });
